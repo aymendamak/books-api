1853b449ba16d042e07d99d311c82cc114b3095c
aymendamak <aymen.damak@yahoo.fr>
2025-01-02 17:34:47 +0100

    Add pino for loggs the http requests

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,23 @@ import express from "express";
 import authorRouter from "./routes/author.router";
 import bookRouter from "./routes/book.router";
 
+import logger from "./utils/logger";
+
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+  logger.info(`${req.method} ${req.path}`);
+  next();
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  logger.error(`Error occurred: ${err.message}`);
+  res.status(500).send("Internal Server Error");
+});
 
 app.use("/authors", authorRouter);
 app.use("/books", bookRouter);

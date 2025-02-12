c5804c87daf23cc10838a4dbc828822f1c3d7f1c
aymendamak <aymendamak2@gmail.com>
2025-02-12 22:09:26 +0100

    Add CORS support and extended URL encoding

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,15 @@ import express from "express";
 
 import authorRouter from "./routes/author.router";
 import bookRouter from "./routes/book.router";
-
+import cors from "cors";
 import logger from "./utils/logger";
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use((req, res, next) => {
   logger.info(`${req.method} ${req.path}`);
